a3f16b94b4ce75aebbb69af24ad22ebb02f952ec
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-15 14:00:36 -0600

    Changing url bracket pattern for deployment.

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import DeviceViews
 
-urlpatterns = {
+urlpatterns = [
     path('v1/devices/<device_id>', DeviceViews.as_view({'get': 'retrieve',
                                                         'put': 'update'})),
-}
+]
