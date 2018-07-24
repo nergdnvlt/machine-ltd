b6df9f2facb0c1b6d458ec0343c39961922859d4
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-24 15:27:17 -0600

    TDD top drive development of distance calculation.

--- V1/locations/Tests/test_location_model.py
+++ V1/locations/Tests/test_location_model.py
@@ -24,3 +24,19 @@ class LocationModelTestCase(TestCase):
         self.assertEqual(saved_location.long, -105.234931)
         self.assertEqual(saved_location.device.id, device.id)
         self.assertEqual(count, 1)
+
+    def test_device_calculates_distance(self):
+        user = User.objects.create(username='Thrasher',
+                                   phone_number='7196639883',)
+        device = Device.objects.create(user=user,
+                                       sms_number='7192710056',
+                                       pin_lat=39.996665,
+                                       pin_long=-105.234931)
+
+        input_location = Location.objects.create(device=device, lat=39.996665, long=-105.234931)
+
+        saved_location = Location.objects.first()
+
+        self.assertEqual(saved_location.lat, 39.996665)
+        self.assertEqual(saved_location.long, -105.234931)
+        self.assertEqual(saved_location.distance, 0)
