9c5188a4f6c24b5b94e7b8156cedc3989ff0af1f
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-26 18:30:51 -0600

    History endpoint  is functional.

--- V1/devices/urls.py
+++ V1/devices/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from V1.devices.views import DeviceViews
+from V1.locations.views import LocationViews
 
 urlpatterns = [
     path('<device_id>', DeviceViews.as_view({'get': 'retrieve', 'post': 'update_location'})),
+    path('<device_id>/history', LocationViews.as_view({'get': 'index'})),
 ]
