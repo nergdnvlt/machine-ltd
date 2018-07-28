c22779badaa376dc9e99bbde319756d2581523a7
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-28 15:53:39 -0600

    Finished endpoint testing. Gonna refactor user model.

--- V1/users/Tests/test_user_endpoints.py
+++ V1/users/Tests/test_user_endpoints.py
@@ -45,7 +45,7 @@ class UserEndpointTest(TestCase):
         response = self.client.post('/api/v1/users/', user, format='json')
         user = response.json()
 
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
     def test_get_user_endpoint(self):
@@ -70,50 +70,97 @@ class UserEndpointTest(TestCase):
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
 
-    # def test_update_put_user_endpoint(self):
-    #     db_user = User.objects.create(username="Thrasher", phone_number="+17192710056")
-    #
-    #     user = {
-    #         "username": "Fluffy",
-    #         "phone_number": "+17196639883"
-    #
-    #     }
-    #
-    #     response = self.client.put(f'/api/v1/users/{db_user.id}', user, format='json')
-    #     end_user = response.json()
-    #
-    #     self.assertEqual(end_user['username'], 'Fluffy')
-    #     self.assertEqual(end_user['phone_number'], '+17192710056')
+    def test_update_put_user_endpoint(self):
+        db_user = User.objects.create(username="Thrasher", phone_number="+17192710056")
+
+        user = {
+            "username": "Fluffy",
+            "phone_number": "+17196639883"
+
+        }
+
+        response = self.client.put(f'/api/v1/users/{db_user.id}', user, format='json')
+        end_user = response.json()
+
+        self.assertEqual(end_user['username'], 'Fluffy')
+        self.assertEqual(end_user['phone_number'], '+17196639883')
 
 
-    # def test_sad_path_put_user_endpoint(self):
-    #     response = self.client.put('/api/v1/users/10001', {'user': {'username': 'Thor', 'phone_number': '+17195558888'}}, format='json')
-    #
-    #     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-    #
-    # def test_patch_user_endpoint(self):
-    #     self.client.post('/api/v1/users/', {'user': {'username': 'Thor', 'phone_number': '+17195558888'}}, format='json')
-    #     user_id = User.objects.last().id
-    #
-    #     response = self.client.patch(f'/api/v1/users/{user_id}', {'user': {'username': 'Thor', 'phone_number': '+17192710056'}}, format='json')
-    #     end_user = response.json()
-    #
-    #     self.assertEqual(end_user['phone_number'], '+17192710056')
-    #
-    # def test_sad_path_patch_user_endpoint(self):
-    #     response = self.client.patch('/api/v1/users/10001', {'user': {'username': 'Thor', 'phone_number': '+17192291210'}}, format='json')
-    #
-    #     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
-    #
-    # def test_delete_user_endpoint(self):
-    #     self.client.post('/api/v1/users/', {'user': {'username': 'Thor', 'phone_number': '+17195558888'}}, format='json')
-    #     user_id = User.objects.last().id
-    #
-    #     response = self.client.delete(f'/api/v1/users/{user_id}')
-    #
-    #     self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-    #
-    # def test_sad_path_delete_user_endpoint(self):
-    #     response = self.client.delete(f'/api/v1/users/10001')
-    #
-    #     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    def test_put_user_endpoint_no_username(self):
+        db_user = User.objects.create(username="Thor", phone_number="+17195558888")
+        user = {
+            "phone_number": "+17192710056"
+        }
+        response = self.client.put(f'/api/v1/users/{db_user.id}', user, format='json')
+        end_user = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_putput_user_endpoint_no_phone(self):
+        db_user = User.objects.create(username="Thor", phone_number="+17195558888")
+        user = {
+            "username": "Thrasher",
+        }
+        response = self.client.put(f'/api/v1/users/{db_user.id}', user, format='json')
+        end_user = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_sad_path_put_user_endpoint(self):
+        response = self.client.put('/api/v1/users/10001', {'user': {'username': 'Thor', 'phone_number': '+17195558888'}}, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    def test_patch_user_endpoint(self):
+        db_user = User.objects.create(username="Thor", phone_number="+17195558888")
+        user = {
+            "username": "Thor",
+            "phone_number": "+17192710056"
+        }
+        response = self.client.patch(f'/api/v1/users/{db_user.id}', user, format='json')
+        end_user = response.json()
+
+        self.assertEqual(end_user['phone_number'], '+17192710056')
+
+
+    def test_patch_user_endpoint_no_username(self):
+        db_user = User.objects.create(username="Thor", phone_number="+17195558888")
+        user = {
+            "phone_number": "+17192710056"
+        }
+        response = self.client.patch(f'/api/v1/users/{db_user.id}', user, format='json')
+        end_user = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+    def test_patch_user_endpoint_no_phone(self):
+        db_user = User.objects.create(username="Thor", phone_number="+17195558888")
+        user = {
+            "username": "Thrasher",
+        }
+        response = self.client.patch(f'/api/v1/users/{db_user.id}', user, format='json')
+        end_user = response.json()
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_sad_path_patch_user_endpoint(self):
+        response = self.client.patch('/api/v1/users/10001', {'user': {'username': 'Thor', 'phone_number': '+17192291210'}}, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_delete_user_endpoint(self):
+        self.client.post('/api/v1/users/', {'user': {'username': 'Thor', 'phone_number': '+17195558888'}}, format='json')
+        user_id = User.objects.last().id
+
+        response = self.client.delete(f'/api/v1/users/{user_id}')
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_sad_path_delete_user_endpoint(self):
+        response = self.client.delete(f'/api/v1/users/10001')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
