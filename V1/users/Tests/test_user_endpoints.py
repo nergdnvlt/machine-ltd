3af6ad809be177d120a3fefc002953fd57504246
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-28 15:32:49 -0600

    Refactoring test to allow routing through serializer.

--- V1/users/Tests/test_user_endpoints.py
+++ V1/users/Tests/test_user_endpoints.py
@@ -28,41 +28,42 @@ class UserEndpointTest(TestCase):
         self.assertEqual(user['phone_number'], '+17196639883')
 
 
-    # def test_get_user_endpoint(self):
-    #     response = self.client.get(f'/api/v1/users/{self.thrasher.id}')
-    #     user = response.json()
-    #
-    #     self.assertEqual(user['username'], 'Thrasher')
-    #     self.assertEqual(user['phone_number'], '+17196639883')
-    #
-    #
-    # def test_get_another_user_endpoint(self):
-    #     response = self.client.get(f'/api/v1/users/{self.fluffy.id}')
-    #     user = response.json()
-    #
-    #     self.assertEqual(user['username'], 'Fluffy')
-    #     self.assertEqual(user['phone_number'], '+17198839888')
-    #
-    #
-    # def test_get_single_user_sad_path_endpoint(self):
-    #     response = self.client.get('/api/v1/users/10001')
-    #
-    #     self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    def test_get_user_endpoint(self):
+        response = self.client.get(f'/api/v1/users/{self.thrasher.id}')
+        user = response.json()
+
+        self.assertEqual(user['username'], 'Thrasher')
+        self.assertEqual(user['phone_number'], '+17196639883')
 
 
-    # def test_update_put_user_endpoint(self):
-    #     user = {
-    #         "username": "Thrasher",
-    #         "phone_number": "+17196639883"
-    #
-    #     }
-    #     self.client.post('/api/v1/users/', user, format='json')
-    #     user_id = User.objects.last().id
-    #
-    #     response = self.client.put(f'/api/v1/users/{user_id}', {'user': {'username': 'Thor', 'phone_number': '+17192710056'}}, format='json')
-    #     end_user = response.json()
-    #
-    #     self.assertEqual(end_user['phone_number'], '+17192710056')
+    def test_get_another_user_endpoint(self):
+        response = self.client.get(f'/api/v1/users/{self.fluffy.id}')
+        user = response.json()
+
+        self.assertEqual(user['username'], 'Fluffy')
+        self.assertEqual(user['phone_number'], '+17198839888')
+
+
+    def test_get_single_user_sad_path_endpoint(self):
+        response = self.client.get('/api/v1/users/10001')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    def test_update_put_user_endpoint(self):
+        db_user = User.objects.create(username="Thrasher", phone_number="+17192710056")
+
+        user = {
+            "username": "Fluffy",
+            "phone_number": "+17196639883"
+
+        }
+
+        response = self.client.put(f'/api/v1/users/{db_user.id}', user, format='json')
+        end_user = response.json()
+
+        self.assertEqual(end_user['username'], 'Fluffy')
+        self.assertEqual(end_user['phone_number'], '+17192710056')
 
 
     # def test_sad_path_put_user_endpoint(self):
