25c4146da400429922f03a23ae077dde56af34ef
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-28 15:54:32 -0600

    Refactoring user model for unnecessary methods.

--- V1/users/models.py
+++ V1/users/models.py
@@ -5,13 +5,5 @@ class User(models.Model):
     username = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100)
 
-    def update_user(self, user_attrs):
-        if 'username' in user_attrs.keys():
-            self.username = user_attrs['username']
-        if 'phone_number' in user_attrs.keys():
-            self.phone_number = user_attrs['phone_number']
-        self.save()
-        return self
-
     class Meta:
         db_table = 'users'
