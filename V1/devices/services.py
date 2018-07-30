4b732bb1de52e932cd132de5f3af3fda6fcd1ad4
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-30 10:07:34 -0600

    Adding service to device for create and update.

--- V1/devices/services.py
+++ V1/devices/services.py
@@ -1,5 +1,10 @@
 from twilio.rest import Client
+from V1.devices.models import Device
+from django.shortcuts import get_object_or_404
 from service_objects.services import Service
+from V1.devices.serializers import DeviceSerializer
+from rest_framework.response import Response
+from rest_framework import status
 from django.conf import settings
 
 class TwilioService(Service):
@@ -17,3 +22,27 @@ class TwilioService(Service):
             body=f'Moving asset. Location: lattitude: {lat}, and longitude {long}. {url}'
         )
         return message.body
+
+class DeviceService(Service):
+    def __init__(self):
+        None
+
+    def create_device(self, request):
+        serializer = DeviceSerializer(data=request.data)
+        if serializer.is_valid():
+            device = serializer.save()
+            if device.id:
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def update_device(self, request, device_id):
+        device = get_object_or_404(Device, id=device_id)
+        serializer = DeviceSerializer(data=request.data)
+        if serializer.is_valid():
+            device = serializer.update(instance=device, validated_data=serializer.data)
+            if device:
+                res_serializer = DeviceSerializer(device, many=False)
+                return Response(res_serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- V1/devices/views.py
+++ V1/devices/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from V1.devices.models import Device
-from V1.locations.models import Location
 from V1.devices.serializers import DeviceSerializer
-from V1.locations.serializers import LocationSerializer
+from V1.devices.services import DeviceService
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
-import json
+
 
 class DeviceViews(viewsets.ViewSet):
 
@@ -17,29 +16,13 @@ class DeviceViews(viewsets.ViewSet):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request):
-        serializer = DeviceSerializer(data=request.data)
-        if serializer.is_valid():
-            device = serializer.save()
-            if device.id:
-                return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        create_response = DeviceService().create_device(request)
+        return create_response
 
     def update(self, request, device_id=None):
-        device_info = self.__device_update__(request, device_id)
+        device_info = DeviceService().update_device(request, device_id)
         return device_info
 
     def partial_update(self, request, device_id=None):
-        device_info = self.__device_update__(request, device_id)
+        device_info = DeviceService().update_device(request, device_id)
         return device_info
-
-    def __device_update__(self, request, device_id):
-        device = get_object_or_404(Device, id=device_id)
-        serializer = DeviceSerializer(data=request.data)
-        if serializer.is_valid():
-            device = serializer.update(instance=device, validated_data=serializer.data)
-            if device:
-                res_serializer = DeviceSerializer(device, many=False)
-                return Response(res_serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
