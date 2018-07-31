9f3d205adf7a2a8b23a8b2144c149e4733d57c79
nergdvnlt <tylerlundgren@yahoo.com>
2018-07-31 14:45:01 -0600

    Refactoring for sessions.

--- V1/urls.py
+++ V1/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from V1.views import SessionViews
 
 urlpatterns = [
     path('devices/', include('V1.devices.urls')),
     path('users/', include('V1.users.urls')),
     path('locations/', include('V1.locations.urls')),
+    path('sessions/', SessionViews.as_view({
+        'post': 'create',
+    })),
 ]
--- V1/views.py
+++ V1/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from V1.users.models import User
+from V1.users.serializers import UserSerializer
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.response import Response
+import json
 
-# Create your views here.
+class SessionViews(viewsets.ViewSet):
+    None
